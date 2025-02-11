e4e78ee5a98925eb6c920bef73fbf305c3fff94d
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-02-11 14:22:35 +0100

    prepare 1.21.4 port

---
+++ 1.21.4/NeoForge/src/main/java/fuzs/hoveringhotbar/neoforge/client/HoveringHotbarNeoForgeClient.java
@@ -0,0 +1,30 @@
+package fuzs.hoveringhotbar.neoforge.client;
+
+import fuzs.hoveringhotbar.HoveringHotbar;
+import fuzs.hoveringhotbar.client.HoveringHotbarClient;
+import fuzs.hoveringhotbar.data.client.ModLanguageProvider;
+import fuzs.hoveringhotbar.neoforge.client.handler.HotbarShiftHandler;
+import fuzs.puzzleslib.api.client.core.v1.ClientModConstructor;
+import fuzs.puzzleslib.neoforge.api.data.v2.core.DataProviderHelper;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.EventPriority;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.NeoForge;
+
+@Mod(value = HoveringHotbar.MOD_ID, dist = Dist.CLIENT)
+public class HoveringHotbarNeoForgeClient {
+
+    public HoveringHotbarNeoForgeClient() {
+        ClientModConstructor.construct(HoveringHotbar.MOD_ID, HoveringHotbarClient::new);
+        registerEventHandlers(NeoForge.EVENT_BUS);
+        DataProviderHelper.registerDataProviders(HoveringHotbar.MOD_ID, ModLanguageProvider::new);
+    }
+
+    private static void registerEventHandlers(IEventBus eventBus) {
+        // try to not push a pose on the stack when the event is cancelled
+        eventBus.addListener(EventPriority.LOW, HotbarShiftHandler::onBeforeRenderGui);
+        eventBus.addListener(HotbarShiftHandler::onAfterRenderGui);
+        eventBus.addListener(HotbarShiftHandler::onBeforeRenderGuiLayer);
+    }
+}
