5e8aed9ae887a154b10707eb55e1ffdb9baaa7f2
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-04-23 19:33:26 +0200

    full 1.21.5 port

--- 1.21.5/Common/src/main/java/fuzs/hoveringhotbar/client/helper/HotbarSpriteHelper.java
+++ 1.21.5/Common/src/main/java/fuzs/hoveringhotbar/client/helper/HotbarSpriteHelper.java
@@ -1,23 +1,47 @@
 package fuzs.hoveringhotbar.client.helper;
 
-import com.mojang.blaze3d.systems.RenderSystem;
-import fuzs.puzzleslib.api.client.gui.v2.components.GuiGraphicsHelper;
+import com.google.common.collect.ImmutableList;
+import fuzs.hoveringhotbar.HoveringHotbar;
+import fuzs.hoveringhotbar.config.ClientConfig;
+import fuzs.puzzleslib.api.client.core.v1.context.GuiLayersContext;
+import fuzs.puzzleslib.api.client.gui.v2.GuiGraphicsHelper;
+import fuzs.puzzleslib.api.client.gui.v2.GuiHeightHelper;
+import fuzs.puzzleslib.api.core.v1.ModLoaderEnvironment;
+import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.LayeredDraw;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 
+import java.util.List;
+
 public class HotbarSpriteHelper {
     private static final ResourceLocation HOTBAR_SELECTION_SPRITE = ResourceLocation.withDefaultNamespace(
             "hud/hotbar_selection");
+    static final List<ResourceLocation> HOTBAR_GUI_LAYER_LOCATIONS = ImmutableList.of(GuiLayersContext.HOTBAR,
+            GuiLayersContext.JUMP_METER,
+            GuiLayersContext.EXPERIENCE_BAR,
+            GuiLayersContext.EXPERIENCE_LEVEL);
+    static final List<ResourceLocation> FABRIC_HOTBAR_GUI_LAYER_LOCATIONS = ImmutableList.of(GuiLayersContext.EXPERIENCE_LEVEL);
 
-    public static void blitHotbarSelectionSprite(GuiGraphics guiGraphics) {
+    public static List<ResourceLocation> getHotbarGuiLayerLocations() {
+        // other layers are handled via the native event on Fabric, as it offers a gui layer encompassing most hotbar elements,
+        // which has a good chance of also catching anything modded
+        if (ModLoaderEnvironment.INSTANCE.getModLoader().isFabricLike()) {
+            return FABRIC_HOTBAR_GUI_LAYER_LOCATIONS;
+        } else {
+            return HOTBAR_GUI_LAYER_LOCATIONS;
+        }
+    }
+
+    public static void blitHotbarSelectionSprite(GuiGraphics guiGraphics, DeltaTracker deltaTracker) {
         if (Minecraft.getInstance().getCameraEntity() instanceof Player player) {
-            RenderSystem.enableBlend();
             guiGraphics.pose().pushPose();
             guiGraphics.pose().translate(0.0F, 0.0F, -95.0F);
-            int posX = guiGraphics.guiWidth() / 2 - 91 - 1 + player.getInventory().selected * 20;
+            int posX = guiGraphics.guiWidth() / 2 - 91 - 1 + player.getInventory().getSelectedSlot() * 20;
             int posY = guiGraphics.guiHeight() - 22 - 1;
             GuiGraphicsHelper.blitTiledSprite(guiGraphics,
                     RenderType::guiTextured,
@@ -29,7 +53,20 @@ public class HotbarSpriteHelper {
                     24,
                     23);
             guiGraphics.pose().popPose();
-            RenderSystem.disableBlend();
         }
+    }
+
+    public static LayeredDraw.Layer getLayerWithTranslation(LayeredDraw.Layer layer) {
+        return (GuiGraphics guiGraphics, DeltaTracker deltaTracker) -> {
+            guiGraphics.pose().pushPose();
+            guiGraphics.pose().translate(0.0F, -HoveringHotbar.CONFIG.get(ClientConfig.class).getHotbarOffset(), 0.0F);
+            layer.render(guiGraphics, deltaTracker);
+            guiGraphics.pose().popPose();
+        };
+    }
+
+    public static void applyGuiHeight(Gui gui) {
+        GuiHeightHelper.addLeftHeight(gui, HoveringHotbar.CONFIG.get(ClientConfig.class).getHotbarOffset());
+        GuiHeightHelper.addRightHeight(gui, HoveringHotbar.CONFIG.get(ClientConfig.class).getHotbarOffset());
     }
 }
--- 1.21.5/Fabric/src/main/java/fuzs/hoveringhotbar/fabric/client/HoveringHotbarFabricClient.java
+++ 1.21.5/Fabric/src/main/java/fuzs/hoveringhotbar/fabric/client/HoveringHotbarFabricClient.java
@@ -2,13 +2,48 @@ package fuzs.hoveringhotbar.fabric.client;
 
 import fuzs.hoveringhotbar.HoveringHotbar;
 import fuzs.hoveringhotbar.client.HoveringHotbarClient;
+import fuzs.hoveringhotbar.client.helper.HotbarSpriteHelper;
+import fuzs.hoveringhotbar.config.ClientConfig;
 import fuzs.puzzleslib.api.client.core.v1.ClientModConstructor;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.HudLayerRegistrationCallback;
+import net.fabricmc.fabric.api.client.rendering.v1.IdentifiedLayer;
+import net.fabricmc.fabric.api.client.rendering.v1.LayeredDrawerWrapper;
+import net.minecraft.client.DeltaTracker;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.resources.ResourceLocation;
 
 public class HoveringHotbarFabricClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
         ClientModConstructor.construct(HoveringHotbar.MOD_ID, HoveringHotbarClient::new);
+        registerEventHandlers();
+    }
+
+    private static void registerEventHandlers() {
+        // this should ideally use a late event phase for wrapping, add that if it ever becomes an issue
+        HudLayerRegistrationCallback.EVENT.register((LayeredDrawerWrapper layeredDrawerWrapper) -> {
+            layeredDrawerWrapper.replaceLayer(IdentifiedLayer.HOTBAR_AND_BARS, (IdentifiedLayer identifiedLayer) -> {
+                return IdentifiedLayer.of(identifiedLayer.id(),
+                        (GuiGraphics guiGraphics, DeltaTracker deltaTracker) -> {
+                            HotbarSpriteHelper.getLayerWithTranslation(identifiedLayer)
+                                    .render(guiGraphics, deltaTracker);
+                            HotbarSpriteHelper.applyGuiHeight(Minecraft.getInstance().gui);
+                        });
+            });
+            // our gui layer system does not support modded layers, so use the native event here
+            for (ResourceLocation resourceLocation : HoveringHotbar.CONFIG.get(ClientConfig.class).hotbarGuiLayers) {
+                try {
+                    layeredDrawerWrapper.replaceLayer(resourceLocation, (IdentifiedLayer identifiedLayer) -> {
+                        return IdentifiedLayer.of(identifiedLayer.id(),
+                                HotbarSpriteHelper.getLayerWithTranslation(identifiedLayer));
+                    });
+                } catch (Exception exception) {
+                    HoveringHotbar.LOGGER.warn("Failed to replace gui layer {}", resourceLocation);
+                }
+            }
+        });
     }
 }
