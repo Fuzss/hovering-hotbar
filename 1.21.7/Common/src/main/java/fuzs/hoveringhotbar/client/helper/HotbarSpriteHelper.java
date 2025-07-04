55e7e3a5bf8ba0513ba9bac5d65d4b126c5074d6
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-07-05 00:54:12 +0200

    prepare 1.21.7 port

---
+++ 1.21.7/Common/src/main/java/fuzs/hoveringhotbar/client/helper/HotbarSpriteHelper.java
@@ -0,0 +1,76 @@
+package fuzs.hoveringhotbar.client.helper;
+
+import com.google.common.collect.ImmutableList;
+import fuzs.hoveringhotbar.HoveringHotbar;
+import fuzs.hoveringhotbar.config.ClientConfig;
+import fuzs.puzzleslib.api.client.core.v1.context.GuiLayersContext;
+import fuzs.puzzleslib.api.client.gui.v2.GuiGraphicsHelper;
+import fuzs.puzzleslib.api.client.gui.v2.GuiHeightHelper;
+import fuzs.puzzleslib.api.core.v1.ModLoaderEnvironment;
+import net.minecraft.client.DeltaTracker;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.LayeredDraw;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.GameType;
+
+import java.util.List;
+
+public class HotbarSpriteHelper {
+    private static final ResourceLocation HOTBAR_SELECTION_SPRITE = ResourceLocation.withDefaultNamespace(
+            "hud/hotbar_selection");
+    static final List<ResourceLocation> HOTBAR_GUI_LAYER_LOCATIONS = ImmutableList.of(GuiLayersContext.HOTBAR,
+            GuiLayersContext.JUMP_METER,
+            GuiLayersContext.EXPERIENCE_BAR,
+            GuiLayersContext.EXPERIENCE_LEVEL);
+    static final List<ResourceLocation> FABRIC_HOTBAR_GUI_LAYER_LOCATIONS = ImmutableList.of(GuiLayersContext.EXPERIENCE_LEVEL);
+
+    public static List<ResourceLocation> getHotbarGuiLayerLocations() {
+        // other layers are handled via the native event on Fabric, as it offers a gui layer encompassing most hotbar elements,
+        // which has a good chance of also catching anything modded
+        if (ModLoaderEnvironment.INSTANCE.getModLoader().isFabricLike()) {
+            return FABRIC_HOTBAR_GUI_LAYER_LOCATIONS;
+        } else {
+            return HOTBAR_GUI_LAYER_LOCATIONS;
+        }
+    }
+
+    public static void blitHotbarSelectionSprite(GuiGraphics guiGraphics, DeltaTracker deltaTracker) {
+        Minecraft minecraft = Minecraft.getInstance();
+        if (minecraft.gameMode.getPlayerMode() != GameType.SPECTATOR) {
+            if (minecraft.getCameraEntity() instanceof Player player) {
+                guiGraphics.pose().pushPose();
+                guiGraphics.pose().translate(0.0F, 0.0F, -95.0F);
+                int posX = guiGraphics.guiWidth() / 2 - 91 - 1 + player.getInventory().getSelectedSlot() * 20;
+                int posY = guiGraphics.guiHeight() - 22 - 1;
+                GuiGraphicsHelper.blitTiledSprite(guiGraphics,
+                        RenderType::guiTextured,
+                        HOTBAR_SELECTION_SPRITE,
+                        posX,
+                        posY,
+                        24,
+                        24,
+                        24,
+                        23);
+                guiGraphics.pose().popPose();
+            }
+        }
+    }
+
+    public static LayeredDraw.Layer getLayerWithTranslation(LayeredDraw.Layer layer) {
+        return (GuiGraphics guiGraphics, DeltaTracker deltaTracker) -> {
+            guiGraphics.pose().pushPose();
+            guiGraphics.pose().translate(0.0F, -HoveringHotbar.CONFIG.get(ClientConfig.class).getHotbarOffset(), 0.0F);
+            layer.render(guiGraphics, deltaTracker);
+            guiGraphics.pose().popPose();
+        };
+    }
+
+    public static void applyGuiHeight(Gui gui) {
+        GuiHeightHelper.addLeftHeight(gui, HoveringHotbar.CONFIG.get(ClientConfig.class).getHotbarOffset());
+        GuiHeightHelper.addRightHeight(gui, HoveringHotbar.CONFIG.get(ClientConfig.class).getHotbarOffset());
+    }
+}
