9cfbfd26c70b28e461a67671bfc14f35d9595a45
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-07-18 14:39:22 +0200

    prepare 1.21.8 port

---
+++ Common/src/main/java/fuzs/hoveringhotbar/config/ClientConfig.java
@@ -0,0 +1,50 @@
+package fuzs.hoveringhotbar.config;
+
+import com.google.common.collect.ImmutableSet;
+import fuzs.puzzleslib.api.config.v3.Config;
+import fuzs.puzzleslib.api.config.v3.ConfigCore;
+import fuzs.puzzleslib.api.config.v3.ValueCallback;
+import fuzs.puzzleslib.api.core.v1.utility.ResourceLocationHelper;
+import net.minecraft.resources.ResourceLocation;
+import net.neoforged.neoforge.common.ModConfigSpec;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+public class ClientConfig implements ConfigCore {
+    @Config(description = "Move the experience level display above the experience bar.", gameRestart = true)
+    public boolean moveExperienceAboveBar = true;
+    @Config(
+            name = "hotbar_gui_layers",
+            description = "Defines a set of gui layers that should be shifted together with the hotbar."
+    )
+    List<String> hotbarGuiLayersRaw = Collections.emptyList();
+
+    private ModConfigSpec.IntValue hotbarOffsetValue;
+    public Set<ResourceLocation> hotbarGuiLayers;
+
+    public int getHotbarOffset() {
+        return this.hotbarOffsetValue.getAsInt();
+    }
+
+    public void updateHotbarOffset(int screenHeight, boolean moveUp) {
+        this.hotbarOffsetValue.set(Math.clamp(this.getHotbarOffset() + (moveUp ? 1 : -1), 0, screenHeight));
+        this.hotbarOffsetValue.save();
+    }
+
+    @Override
+    public void addToBuilder(ModConfigSpec.Builder builder, ValueCallback callback) {
+        this.hotbarOffsetValue = builder.comment("Height offset for the hotbar from the screen bottom.")
+                .defineInRange("hotbar_offset", 2, 0, Integer.MAX_VALUE);
+    }
+
+    @Override
+    public void afterConfigReload() {
+        this.hotbarGuiLayers = this.hotbarGuiLayersRaw.stream()
+                .map(ResourceLocationHelper::tryParse)
+                .filter(Objects::nonNull)
+                .collect(ImmutableSet.toImmutableSet());
+    }
+}
